3e3b7346e7f3d7015c9a6c7231028b6df07b318d
njiti <kamaukan77@gmail.com>
2022-02-10 16:53:52 -0930

    Components
    Create github-app,github-profile,github-repos,github-profile,github-profile-card,github-profile-data and github-repos component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubAppComponent } from './github-app/github-app.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { GithubProfileCardComponent } from './github-profile-card/github-profile-card.component';
+import { GithubProfileDataComponent } from './github-profile-data/github-profile-data.component';
+import { GithubReposComponent } from './github-repos/github-repos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubAppComponent,
+    GithubProfileComponent,
+    GithubProfileCardComponent,
+    GithubProfileDataComponent,
+    GithubReposComponent
   ],
   imports: [
     BrowserModule,
