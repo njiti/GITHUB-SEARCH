2bb0b8c551ab777935b00098911456381f0eb13f
njiti <kamaukan77@gmail.com>
2022-02-12 10:16:53 -0930

    Added my  clientId and  client Secret
    Added my  clientId and  client Secret

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  clientId: 'Iv1.d04630eefdfd6384',
+  clientSecret: 'c006fbce5c85209043783a30f05638499a92b44b'
 };
 
 /*
