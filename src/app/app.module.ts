ece6eda07fbc5fd5952fa7c76f8fd00259e6d988
njiti <kamaukan77@gmail.com>
2022-02-12 10:01:18 -0930

    AppRouting Module error
    Corrected an app routing error that I was experiencing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { GithubAppComponent } from './github-app/github-app.component';
-import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { GithubProfileComponent} from './github-profile/github-profile.component';
 import { GithubProfileCardComponent } from './github-profile-card/github-profile-card.component';
 import { GithubProfileDataComponent } from './github-profile-data/github-profile-data.component';
 import { GithubReposComponent } from './github-repos/github-repos.component';
+import { GithubService } from './github.service';
 
 @NgModule({
   declarations: [
@@ -20,9 +23,11 @@ import { GithubReposComponent } from './github-repos/github-repos.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [GithubService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
