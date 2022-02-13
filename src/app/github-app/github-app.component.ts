39f405467e6a7cb5fad9fb8a7eb481c163e7d24e
njiti <kamaukan77@gmail.com>
2022-02-13 11:08:29 -0930

    Submit
    Added  a submit function to my  search bar

--- src/app/github-app/github-app.component.ts
+++ src/app/github-app/github-app.component.ts
@@ -1,5 +1,6 @@
 import { error } from '@angular/compiler/src/util';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { GithubService } from '../github.service';
 
 @Component({
@@ -7,38 +8,59 @@ import { GithubService } from '../github.service';
   templateUrl: './github-app.component.html',
   styleUrls: ['./github-app.component.css']
 })
-export class GithubAppComponent implements OnInit {
+export class GithubAppComponent implements OnInit, OnDestroy {
+  gifs: any[] = [];
+  subscription!: Subscription;
 
   public githubUserQuery!: string;
-  public githubProfile: any;
+  public githubProfile: any = 'njiti';
   public githubRepos: any[] | undefined;
   public errorMessage: string | undefined;
+  profileData: any;
 
 
   constructor(private githubService:GithubService) { }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
 
   public searchUser(){
     //get github profile
-    this.githubService.getProfile().subscribe(next, (data) =>{
-      this:githubProfile = data,
-    }, error, (error: string) => {
-      this.errorMessage = error;
+    this.githubService.getProfile(searchQuery).subscribe(next, (data) =>{
+      this.githubProfile = data;
     });
+    /*ngOnInit(): void{
+      this.githubService.getProfile(searchQuery);
+      this.subscription  = this.githubService.getProfile(searchQuery)
+      .subscribe((response: any) => {
+        this.profileData = response;
+      });
+    }
+    ngOnDestroy(){
+      this.subscription.unsubscribe();
+    }
+
+    search(searchTerm: string){
+      if (searchTerm !== ''){
+        this.githubService.searchRepos(searchTerm);
+      }
+    }*/
 
     //get the github repos
     this.githubService.getRepos(this.githubUserQuery).subscribe(next, (data) => {
       this.githubRepos = data;
-    },error, (error: string) => {
-      this.errorMessage = error;
     });
-  }
-
-
-  ngOnInit(): void {
   }
 
 }
 function next(): ((value: any) => void) | Partial<import("rxjs").Observer<any>> | null | undefined {
+  throw new Error('Function not implemented.');
+}
+
+function searchQuery(searchQuery: any, any: any) {
   throw new Error('Function not implemented.');
 }
 
--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,25 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { retry, count, catchError, throwError, Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
+  searchRepos(searchTerm: string) {
+    throw new Error('Method not implemented.');
+  }
 
   constructor(private httpClient: HttpClient) { }
 
   //github profiles
   public getProfile(_searchQuery: any):Observable<any>{
-    let dataURL = 'https://api.github.com/users/${searchQuery}?client_id=${clientId}&client_secret=${clientSecret}';
+    let dataURL = `https://api.github.com/users/${_searchQuery}?client_id=${environment.clientId}&client_secret=${environment.clientSecret}`;
     return this.httpClient.get<any>(dataURL).pipe(
       catchError(this.handleErrors)
     );
   }
 
   //for github repos
-  public getRepos(searchQuery: string):Observable<any>{
-    let dataURL = 'https://api.github.com/users/${searchQuery}/repos?client_id=${clientId}&client_secret=${clientSecret}';
+  public getRepos(_searchQuery: string):Observable<any>{
+    let dataURL = `https://api.github.com/users/${_searchQuery}/repos?client_id=${environment.clientId}&client_secret=${environment.clientSecret}`;
     return this.httpClient.get<any[]>(dataURL).pipe(
       catchError(this.handleErrors)
     );
